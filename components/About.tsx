b107aba3b87465bc1651a57d0f5be697f1b36124
JT <jamestasker2004@gmail.com>
2025-11-21 16:46:00 +1300

    About and Projects

--- components/About.tsx
+++ components/About.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Image from 'next/image';
-import { assets, infoList } from '@/assets/assets'
+import { assets, infoList, toolsData } from '@/assets/assets'
 
 const About = () => {
   return (
@@ -13,16 +13,28 @@ const About = () => {
         </div>
         <div className='flex-1'>
           <p className='mb-10 max-w-2xl'>
-            I am a recent graduate from The University of Auckland
+            I have been programming for over 5 years and I enjoy making games, software and web apps.
           </p>
 
           <ul className='grid grid-cols-1 sm:grid-cols-3 gap-6 max-w-2xl'>
             {infoList.map(({icon, iconDark, title, description}, index) => (
               <li className='border-[0.5px] border-gray-500 rounded-xl p-6 cursor-pointer 
-              hover:bg-[var(--light-hover)] - hover:-translate-y-1 duration-500' key={index}>
+              hover:bg-[var(--lightHover)] hover:-translate-y-1 duration-500' key={index}>
                 <Image src={icon} alt={title} />
                 <h3 className='my-4 font-semibold text-gray-700'>{title}</h3>
                 <p className='text-gray-600 text-sm'>{description}</p>
+              </li>
+            ))}
+          </ul>
+
+          <h4 className='my-6 text-gray-700'>Tools I use</h4>
+
+          <ul className='flex items-center gap-3 sm:gap-5'>
+            {toolsData.map((tool, index) => (
+              <li className='flex items-center justify-center w-12 sm:w-14 aspect-square 
+              border border-gray-400 rounded-lg cursor pointer hover:-translate-y-1 duration-500'
+                key={index}>
+                <Image src={tool} alt='Tool' className='w-5 sm:w-7' />
               </li>
             ))}
           </ul>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,10 @@
-import React, { useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Image from 'next/image';
 import { assets } from '@/assets/assets'
 
 const Navbar = () => {
 
+  const [isScroll, setIsScroll] = useState(false);
   const sideMenuRef = useRef<HTMLUListElement | null>(null);
 
   const openMenu = () => {
@@ -18,16 +19,28 @@ const Navbar = () => {
     }
   };
 
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if (scrollY > 50) {
+        setIsScroll(true);
+      } else {
+        setIsScroll(false);
+      }
+    })
+  },[])
+
   return (
     <>
     <div className='fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]'>
         <Image src={assets.header_bg_color} alt='' className='w-full' />
     </div>
-      <nav className='w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50'>
+      <nav className={`w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center 
+        justify-between z-50 ${isScroll ? "bg-[rgba(255, 255, 255, 0.5)] backdrop-blur-lg shadow-sm" : ""}`}>
         <a href="">
           <Image src={assets.logo} alt='' className='w-28 cursor-pointer mr-14'/>
         </a>
-        <ul className='hidden md:flex item-center gap-6 lg:gap8 rounded-full px-12 py-3 bg-white shadow-sm bg-opacity-50'> 
+        <ul className={`hidden md:flex item-center gap-6 lg:gap8 rounded-full px-12 
+          py-3 ${isScroll ? "" : "shadow-sm bg-[rgba(255, 255, 255, 0.5)]"} `}> 
           <li><a href="#top">Home</a></li>
           <li><a href="#about">About me</a></li>
           <li><a href="#services">Services</a></li>
