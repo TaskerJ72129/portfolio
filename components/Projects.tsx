bab72d3b05a66ed1f4007c3abc986822c5cb9d98
JT <jamestasker2004@gmail.com>
2026-01-05 11:50:51 +1300

    finished projects section

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -25,7 +25,7 @@ const Projects = () => {
             className="group"
           >
             <div key={index}
-              className='aspect-square bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group'
+              className='aspect-video bg-no-repeat bg-contain bg- bg-center rounded-lg relative cursor-pointer group'
               style={{ backgroundImage: `url(${project.bgImage})` }}>
               <div className='bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 
                 flex items-center justify-between duration-500 group-hover:bottom-7'>
