4802f5b8d5d495304209d7d62c7ba47522227795
JT <jamestasker2004@gmail.com>
2026-01-09 10:45:26 +1300

    removed unused section in side navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -48,10 +48,6 @@ const Navbar = () => {
         </ul>
 
         <div className='flex items-center gap-4'>
-          {/* <button>
-            <Image src={assets.moon_icon} alt='' className='w-6' />
-          </button> */}
-
           <a href="#contact" className='hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full 
           ml-4'>Contact <Image src={assets.arrow_icon} alt="" className='w-3' /></a>
           
@@ -71,7 +67,6 @@ const Navbar = () => {
 
           <li><a onClick={closeMenu} href="#top">Home</a></li>
           <li><a onClick={closeMenu} href="#about">About me</a></li>
-          <li><a onClick={closeMenu} href="#services">Services</a></li>
           <li><a onClick={closeMenu} href="#projects">My Projects</a></li>
           <li><a onClick={closeMenu} href="#contact">Contact me</a></li>
         </ul>
